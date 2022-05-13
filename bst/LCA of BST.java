2c3f959060dd2a6ed22459d76df68ee0458df8d7
harsha230 <78947177+harsha230@users.noreply.github.com>
2022-05-13 21:40:40 +0530

    Create LCA of BST.java

---
+++ bst/LCA of BST.java
@@ -0,0 +1,49 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	
+	public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+		if(root==null)
+             return -1;
+         int c=-1;
+         if(root.data==a || root.data==b)
+             return root.data;
+
+        else if(a<root.data && b>root.data || a>root.data && b<root.data)
+         {
+              c=getLCA(root.left,a,b);
+             int d=getLCA(root.right,a,b);
+             if(c==-1 && d==-1)
+                 return -1;
+             else if(c==-1 && d!=-1)
+                 return  d;
+             else if(c!=-1 && d==-1)
+                 return c;
+             else
+                 return root.data;
+         }
+
+          else if(a<root.data &&b<root.data)
+         {
+              c=getLCA(root.left,a,b);
+         }
+        else if(a>root.data &&b>root.data)
+         {
+              c=getLCA(root.right,a,b);
+         }
+
+         if(c!=-1)
+             return c;
+          else
+             return -1;
+
+	}
+}
