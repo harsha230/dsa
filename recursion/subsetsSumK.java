b5dcbf48ed586204286edf9eaefdd268da33ee78
harsha230 <78947177+harsha230@users.noreply.github.com>
2022-05-22 09:15:59 +0530

    Create subsetsSumK.java

---
+++ recursion/subsetsSumK.java
@@ -0,0 +1,35 @@
+public class solution {
+
+	// Return a 2D array that contains all the subsets which sum to k
+	public static int[][] subsetsSumK(int input[], int k) {
+		// Write your code here
+         return helper(input,k,0);
+
+	}
+     public static int[][] helper(int input[], int k,int si) {
+         if(si==input.length)
+         {
+             if(k==0)
+                 return new int[1][0];
+             else
+                 return new int[0][0];
+         }
+     int op2[][]=helper(input,k-input[si],si+1);
+     int op1[][]=helper(input,k,si+1);
+     int output[][]=new int[op1.length+op2.length][];
+     int l=0;
+     for(int i=0;i<op1.length;i++){
+         output[l][j]=op2[i];
+         l++;
+     }
+     for(int i=0;i<op2.length;i++){
+         output[l]=new int[op2[i].length+1];
+         output[l][0]=input[si];
+         for(int j=0;j<op1[i].length;j++){
+             output[l][j+1]=op2[i][j];
+         }
+       l++;
+     }
+         return output;}
+}
+
