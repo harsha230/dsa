311850a401352d1d9740271b5a1355103f455333
harsha230 <78947177+harsha230@users.noreply.github.com>
2022-05-28 11:19:02 +0530

    Create ReverseString.java

---
+++ strings/ReverseString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ReverseString {
+    public static String reverseString(String str){
+        String reversedString="";
+        //for(int i=str.length()-1;i>=0;i--){
+          // reversedString+=str.charAt(i);
+      //  }
+      for(int i=0;i<str.length();i++){
+          reversedString+=str.charAt(i);
+      }
+      return reversedString;
+      }
+        
+    
+    
+    
+    
+    
+    public static void main(String args[]) {
+      
+      String str="abcde";
+      String reversedString=reverseString(str);
+      System.out.println(reversedString);
+      
+    }
+}
